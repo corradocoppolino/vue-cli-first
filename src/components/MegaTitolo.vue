ff089ce53b15b092599afc63acffac8fadd7f3f0
corradocoppo <corradocoppolino@gmail.com>
2021-05-20 00:54:07 +0200

    titolo fatto

---
+++ src/components/MegaTitolo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MegaTitolo"
+}
+</script>
+
+<style>
+
+  div{
+    background-color: cornflowerblue;
+    width: 70%;
+    margin: auto;
+    padding: 5px;
+  }
+  h1{
+    text-align: center;
+    color: white;
+    font-size: 40px;
+  }
+</style>
